005f027407dcbc56ab6d486d06e4bbff86632d6e
Lucas Hiroshi Horinouchi <lucas.hh@hotmail.com>
2024-05-31 04:21:26 -0400

    Update app.py

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.markdown("""# This is a header
+st.markdown("""# This is a header NEW
 ## This is a sub header
 ###  This is a sub sub header
 This is text
